067daf59439e9d07825a4ee7755df1b2b86f9448
Kevin Blair <kblair40@gmail.com>
2022-10-15 09:11:27 -0500

    Add DELETE route for study sessions/history

--- src/routes/historyRoutes.js
+++ src/routes/historyRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const { requireAuth } = require("../middleware/requireAuth");
-// const StudySession = mongoose.model("StudySession");
+const StudySession = mongoose.model("StudySession");
 
 const router = express.Router();
 router.use(requireAuth);
@@ -20,6 +20,47 @@ router.get("/history", async (req, res) => {
     });
     console.log("\n\nUSER:", pop_user.study_sessions);
     return res.status(200).send({ history: pop_user.study_sessions });
+  } catch (e) {
+    console.log("error");
+    return res.status(500).send({ msg: "Something went wrong" });
+  }
+});
+
+router.delete("/history/:id", async (req, res) => {
+  const {
+    user,
+    params: { id },
+  } = req;
+  if (!user || !id) {
+    return res.status(422).send({ msg: "User or id is missing" });
+  }
+
+  try {
+    let itemIdx = user.study_sessions.findIndex((ss) => ss._id === id);
+    if (itemIdx === -1) {
+      console.log("\n\n404\n\n");
+      return res
+        .status(404)
+        .send({ msg: "Could not find session id in user's study sessions" });
+    }
+
+    user.study_sessions.splice(itemIdx);
+
+    const [dontcare, updatedUser] = await Promise.all([
+      StudySession.deleteOne({ _id: id }),
+      user.save(),
+    ]);
+
+    await updatedUser.populate({
+      path: "study_sessions",
+      populate: {
+        path: "flashcard_set",
+        model: "FlashcardSet",
+      },
+    });
+
+    console.log("\n\nUPDATED SESSIONS:", updatedUser.study_sessions);
+    return res.status(200).send({ history: updatedUser.study_sessions });
   } catch (e) {
     console.log("error");
     return res.status(500).send({ msg: "Something went wrong" });
