b08ba7d1d982c9b41dcf613e2609c5c25014bbeb
Kevin Blair <kblair40@gmail.com>
2022-10-19 10:34:51 -0500

    Add functionality for deleting a favorited set

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -20,17 +20,31 @@ router.get("/user", async (req, res) => {
   return res.status(200).send({ user });
 });
 
-router.patch("/user", async (req, res) => {
-  console.log(0);
+router.patch("/user/:action", async (req, res) => {
   const { user } = req;
+  const { action } = req.params;
+
   if (!user || !user.flashcard_sets) {
     return res.status(500).send({ msg: "Failure" });
   }
 
   const data = req.body;
+  console.log("DATA:", data);
 
-  if (data.new_favorite_set) {
-    user.favorite_flashcard_sets.push(data.new_favorite_set);
+  if (data.favorite_set) {
+    if (action === "add") {
+      user.favorite_flashcard_sets.push(data.favorite_set);
+    } else {
+      let fav_set_idx = user.favorite_flashcard_sets.findIndex((setId) => {
+        return setId.toString() === data.favorite_set;
+      });
+
+      if (fav_set_idx !== -1) {
+        let sets_copy = [...user.favorite_flashcard_sets];
+        sets_copy.splice(fav_set_idx, 1);
+        user.favorite_flashcard_sets = sets_copy;
+      }
+    }
   }
 
   try {
